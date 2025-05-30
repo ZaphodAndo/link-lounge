54fec583c59f4632734f7b9ea7bdb7905562f2d9
Ethan Anderson <ethan.a@pebblepad.co.uk>
2025-05-30 13:10:35 +0100

    Added initial HTML markup and basic data fetching functionality

---
+++ index.js
@@ -0,0 +1,30 @@
+const response = await fetch("./data.json");
+const data = await response.json();
+const list = document.getElementById("list");
+
+for (let item of data) {
+    const node = document.createElement("li");
+    node.innerHTML = `
+        <h2>${item.title}</h2>
+        <a href="${item.url}" target="_blank">${item.url}</a>
+        <p>${item.note}</p>
+    `;
+    list.appendChild(node);
+}
+
+// Update to reset display: none
+function search(event) {
+    event.preventDefault();
+    const searchTerm = document.getElementById("search").value;
+    const links = list.querySelectorAll("li");
+
+    for (let link of links) {
+        const html = link.getHTML().toLowerCase();
+        if (!html.includes(searchTerm)) {
+            link.style.display = "none";
+        }
+    }
+}
+
+document.getElementById("search-form").addEventListener("submit", search);
+document.getElementById("search-button").addEventListener("click", search);
